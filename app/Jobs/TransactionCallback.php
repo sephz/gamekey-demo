e2ca8d70b105bd4ad411ab52797f1f76679dd2f2
KJ Chew <kj.chew@imttech.co>
2022-07-04 20:31:38 +0800

    Done, pending tests

--- app/Jobs/TransactionCallback.php
+++ app/Jobs/TransactionCallback.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Merchant;
 use App\Models\Transaction;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -35,5 +36,29 @@ class TransactionCallback implements ShouldQueue
     public function handle()
     {
         //get the callback url
+        $merchant = $this->transaction->merchant();
+
+        // get key
+        $key = $this->transaction->key();
+
+        $body = [
+            'transaction_id' => $this->transaction->transaction_id,
+            'key' => $key->key,
+            'total_paid' => $this->transaction->total_paid,
+            'commission' => $this->transaction->commission,
+            'cc_last' => $this->transaction->cc_last,
+            'paid_at' => $this->transaction->paid_at,
+            'status' => $this->transaction->status,
+        ];
+
+        $signature = base64_encode(hash_hmac('sha256', json_encode($body), $merchant->secret, true ));
+
+        $response = Http::timeout(5)->withHeaders(['signature' => $signature])->post($merchant->callback_url, $body);
+
+        if ( $response->body() !== 'OK' ){
+            //retry 10 minutes later
+            self::dispatch($this->transaction)->delay(now()->addMinutes(10));
+        }
+
     }
 }
